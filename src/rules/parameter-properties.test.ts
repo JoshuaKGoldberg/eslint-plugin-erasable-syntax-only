6c9db62fcfb01e16210ec9c858aacaaf8b4b2abe
Alex Munoz <almunozdo@gmail.com>
2025-02-18 22:42:42 +0100

    wip

--- src/rules/parameter-properties.test.ts
+++ src/rules/parameter-properties.test.ts
@@ -23,9 +23,12 @@ ruleTester.run("parameter-properties", rule, {
 							messageId: "parameterPropertyFix",
 							output: `
 				class Values {
+					private value: number;
 					constructor(
 						value: number,
-					) {}
+					) {
+						this.value = value;
+					}
 				}
 			`,
 						},
@@ -35,7 +38,7 @@ ruleTester.run("parameter-properties", rule, {
 		},
 		{
 			code: `
-				class Values {
+				class A {
 					constructor(
 						readonly value: number,
 					) {}
@@ -52,10 +55,136 @@ ruleTester.run("parameter-properties", rule, {
 						{
 							messageId: "parameterPropertyFix",
 							output: `
-				class Values {
+				class A {
+					readonly value: number;
 					constructor(
 						value: number,
+					) {
+						this.value = value;
+					}
+				}
+			`,
+						},
+					],
+				},
+			],
+		},
+		{
+			code: `
+				class Values {
+					protected foo: number;
+					constructor(
+						readonly value: number,
+					) {
+						this.value = value * 2;
+					}
+				}
+			`,
+			errors: [
+				{
+					messageId: "parameterProperty",
+					suggestions: [
+						{
+							messageId: "parameterPropertyFix",
+							output: `
+				class Values {
+					readonly value: number;
+					protected foo: number;
+					constructor(
+						value: number,
+					) {
+						this.value = value * 2;
+					}
+				}
+			`,
+						},
+					],
+				},
+			],
+		},
+		{
+			code: `
+				const Values = class {
+					constructor(
+						private value: number
 					) {}
+				}
+			`,
+			errors: [
+				{
+					messageId: "parameterProperty",
+					suggestions: [
+						{
+							messageId: "parameterPropertyFix",
+							output: `
+				const Values = class {
+					private value: number;
+					constructor(
+						value: number
+					) {
+						this.value = value;
+					}
+				}
+			`,
+						},
+					],
+				},
+			],
+		},
+		{
+			code: `
+				class Values {
+					constructor(
+						readonly value: number
+					) {
+						this.foo = 1;
+					}
+				}
+			`,
+			errors: [
+				{
+					messageId: "parameterProperty",
+					suggestions: [
+						{
+							messageId: "parameterPropertyFix",
+							output: `
+				class Values {
+					readonly value: number;
+					constructor(
+						value: number
+					) {
+						this.value = value;
+						this.foo = 1;
+					}
+				}
+			`,
+						},
+					],
+				},
+			],
+		},
+		{
+			code: `
+				class Values {
+					constructor(
+						private value: number = 3
+					) {}
+				}
+			`,
+			errors: [
+				{
+					messageId: "parameterProperty",
+					suggestions: [
+						{
+							messageId: "parameterPropertyFix",
+							output: `
+				class Values {
+					private value: number;
+					constructor(
+						value: number = 3
+					) {
+						this.value = value;
+					}
 				}
 			`,
 						},
--- src/rules/parameter-properties.ts
+++ src/rules/parameter-properties.ts
@@ -1,19 +1,95 @@
+import { AST_NODE_TYPES } from "@typescript-eslint/utils";
+
 import { createRule } from "../utils.js";
+import { RuleFix } from "@typescript-eslint/utils/ts-eslint";
+import { isIdentifier } from "@typescript-eslint/utils/ast-utils";
 
 export const rule = createRule({
 	create(context) {
 		return {
 			TSParameterProperty(node) {
+				const paramNode = node.parameter;
+				if (
+					paramNode.type !== AST_NODE_TYPES.Identifier &&
+					paramNode.type !== AST_NODE_TYPES.AssignmentPattern
+				) {
+					return;
+				}
+
+				const classBody = node.parent.parent.parent;
+				if (classBody?.type !== AST_NODE_TYPES.ClassBody) {
+					return;
+				}
+
+				const constructor = node.parent;
+				if (constructor.type !== AST_NODE_TYPES.FunctionExpression) {
+					return;
+				}
+
+				let paramName = "";
+				let typeAnnotation = "";
+				const accessibility = node.accessibility
+					? `${node.accessibility} `
+					: "";
+				const readonly = node.readonly ? "readonly " : "";
+				if (
+					paramNode.type === AST_NODE_TYPES.AssignmentPattern &&
+					isIdentifier(paramNode.left)
+				) {
+					paramName = paramNode.left.name;
+					typeAnnotation = context.sourceCode.getText(
+						paramNode.left.typeAnnotation,
+					);
+				} else if (paramNode.type === AST_NODE_TYPES.Identifier) {
+					paramName = paramNode.name;
+					typeAnnotation = context.sourceCode.getText(paramNode.typeAnnotation);
+				}
+				const fieldDeclaration = `${accessibility}${readonly}${paramName}${typeAnnotation};`;
+
+				const isAssignedInConstructor = constructor.body.body.some(
+					(node) =>
+						node.type === AST_NODE_TYPES.ExpressionStatement &&
+						node.expression.type === AST_NODE_TYPES.AssignmentExpression &&
+						node.expression.left.type === AST_NODE_TYPES.MemberExpression &&
+						node.expression.left.object.type ===
+							AST_NODE_TYPES.ThisExpression &&
+						node.expression.left.property.type === AST_NODE_TYPES.Identifier &&
+						node.expression.left.property.name === paramName,
+				);
+
+				const isConstructorBodyEmpty = constructor.body.body.length === 0;
+
 				context.report({
 					messageId: "parameterProperty",
 					node,
 					suggest: [
 						{
 							fix(fixer) {
-								return fixer.removeRange([
-									node.range[0],
-									node.parameter.range[0],
-								]);
+								const fixes: RuleFix[] = [];
+								fixes.push(
+									fixer.insertTextBefore(
+										classBody.body[0],
+										`${fieldDeclaration}\n`,
+									),
+								);
+
+								const parameter = context.sourceCode.getText(paramNode);
+								fixes.push(fixer.replaceText(node, parameter));
+
+								if (!isAssignedInConstructor) {
+									const constructorBody = constructor.body;
+									let assignmentStatement = `this.${paramName} = ${paramName};`;
+									assignmentStatement += isConstructorBodyEmpty ? "\n" : "";
+
+									fixes.push(
+										fixer.insertTextAfterRange(
+											[constructorBody.range[0], constructorBody.range[0] + 1],
+											`\n${assignmentStatement}`,
+										),
+									);
+								}
+
+								return fixes;
 							},
 							messageId: "parameterPropertyFix",
 						},
