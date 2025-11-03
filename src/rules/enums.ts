a608ed6d84bb195e009ebed2792648a12258322f
naremloa <naremloa@gmail.com>
2025-11-04 00:55:15 +0800

    feat: add suggestion to enum rule (#234)
    <!-- 👋 Hi, thanks for sending a PR to
    eslint-plugin-erasable-syntax-only! ❎
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #8 
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/eslint-plugin-erasable-syntax-only/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/eslint-plugin-erasable-syntax-only/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->
    
    Add a suggestion to convert enums into object literals.
    
    
    🎃

--- src/rules/enums.ts
+++ src/rules/enums.ts
@@ -1,12 +1,77 @@
+import { AST_NODE_TYPES } from "@typescript-eslint/utils";
+
 import { createRule } from "../utils.js";
 
 export const rule = createRule({
 	create(context) {
 		return {
 			TSEnumDeclaration(node) {
+				const name = node.id.name;
+				const isExported =
+					node.parent.type === AST_NODE_TYPES.ExportNamedDeclaration;
+				let canSuggestion = true;
+				const body: { content: string; range: [number, number] }[] = [];
+
+				let previousMemberNumericValue = -1;
+				for (const enumMember of node.body.members) {
+					if (
+						enumMember.id.type !== AST_NODE_TYPES.Identifier &&
+						(enumMember.id.type !== AST_NODE_TYPES.Literal ||
+							typeof enumMember.id.value !== "string")
+					) {
+						canSuggestion = false;
+						break;
+					}
+					const propertyName =
+						enumMember.id.type === AST_NODE_TYPES.Identifier
+							? enumMember.id.name
+							: enumMember.id.value;
+					const value =
+						enumMember.initializer?.type === AST_NODE_TYPES.Literal
+							? enumMember.initializer.value
+							: previousMemberNumericValue + 1;
+
+					if (value === null) {
+						canSuggestion = false;
+						break;
+					}
+
+					body.push({
+						content: `${propertyName}: ${typeof value === "string" ? `'${value}'` : value.toString()}`,
+						range: enumMember.range,
+					});
+					if (typeof value === "number") {
+						previousMemberNumericValue = value;
+					}
+				}
+
 				context.report({
 					messageId: "enum",
 					node,
+					suggest: canSuggestion
+						? [
+								{
+									fix(fixer) {
+										return [
+											fixer.replaceTextRange(
+												[node.range[0], node.range[0] + 4],
+												"const",
+											),
+											fixer.insertTextBefore(node.body, "= "),
+											...body.map(({ content, range }) =>
+												fixer.replaceTextRange(range, content),
+											),
+											fixer.insertTextAfter(node.body, " as const"),
+											fixer.insertTextAfter(
+												node.parent.parent ?? node.parent,
+												`\n\n${isExported ? "export " : ""}type ${name} = typeof ${name}[keyof typeof ${name}]`,
+											),
+										];
+									},
+									messageId: "enumFix",
+								},
+							]
+						: null,
 				});
 			},
 		};
@@ -16,8 +81,10 @@ export const rule = createRule({
 		docs: {
 			description: "Avoid using TypeScript's enums.",
 		},
+		hasSuggestions: true,
 		messages: {
 			enum: "This enum will not be allowed under TypeScript's --erasableSyntaxOnly.",
+			enumFix: "Replace enum with an equivalent object literal.",
 		},
 		schema: [],
 		type: "problem",
